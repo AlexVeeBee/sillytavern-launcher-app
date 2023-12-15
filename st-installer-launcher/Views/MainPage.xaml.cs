09f1890877bc89319342a79dba40219a66b4c993
AlexVeeBee <alex200.veebee@gmail.com>
2023-12-15 17:32:22 +0000

    Basic Home page

--- st-installer-launcher/Views/MainPage.xaml.cs
+++ st-installer-launcher/Views/MainPage.xaml.cs
@@ -16,4 +16,33 @@ public sealed partial class MainPage : Page
         ViewModel = App.GetService<MainViewModel>();
         InitializeComponent();
     }
+
+    private void ButtonCheckApps(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+
+    }
+
+    private void AppLuncherSizechange(object sender, Microsoft.UI.Xaml.SizeChangedEventArgs e)
+    {
+        if (e.NewSize.Width < 600)
+        {
+            AppLuncher.Orientation = Orientation.Vertical;
+        }
+        else
+        {
+            AppLuncher.Orientation = Orientation.Horizontal;
+        }
+    }
+
+    private void Home_AppSettings_SizeChanged(object sender, Microsoft.UI.Xaml.SizeChangedEventArgs e)
+    {
+        if (e.NewSize.Width < 600)
+        {
+            Home_AppSettings.Orientation = Orientation.Vertical;
+        }
+        else
+        {
+            Home_AppSettings.Orientation = Orientation.Horizontal;
+        }
+    }
 }
--- st-installer-launcher/obj/x86/Debug/net7.0-windows10.0.19041.0/win10-x86/st-installer-launcher.AssemblyInfo.cs
+++ st-installer-launcher/obj/x86/Debug/net7.0-windows10.0.19041.0/win10-x86/st-installer-launcher.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("st-installer-launcher")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13316dd21528b4083925e2881084e55d855f2024")]
 [assembly: System.Reflection.AssemblyProductAttribute("st-installer-launcher")]
 [assembly: System.Reflection.AssemblyTitleAttribute("st-installer-launcher")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
